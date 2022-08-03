4a42b57d76e611c471cf47ec9ca2c900a8c7ad29
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2022-08-03 13:00:01 -0500

    Cambio de image isHover en ProductCard

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,5 +1,5 @@
-import { Grid, Card, CardActionArea, CardMedia } from '@mui/material'
-import { FC } from 'react';
+import { Grid, Card, CardActionArea, CardMedia, Box, Typography } from '@mui/material'
+import { FC, useMemo, useState } from 'react';
 import { IProducts } from '../../interfaces';
 
   interface ProductCardProps {
@@ -8,17 +8,37 @@ import { IProducts } from '../../interfaces';
 
 
 export const ProductCard:FC<ProductCardProps> = ({product}) => {
+
+  const [isHovered, setisHovered] = useState(false)
+
+  const productImage = useMemo( () => {
+    return isHovered
+    ? `products/${product.images[1] }`
+    : `products/${product.images[0] }`
+  }, [isHovered, product.images]);
+
   return (
-    <Grid item xs={6} sm={4} key={product.slug}>
+    <Grid item
+      xs={6}
+      sm={4}
+      onMouseEnter={() => setisHovered(true)}
+      onMouseLeave={() => setisHovered(false)}
+    >
         <Card>
             <CardActionArea>
                 <CardMedia
                     component='img'
-                    image={`products/${product.images[0] }`}
+                    image={productImage}
                     alt={product.title}
+
                 />
             </CardActionArea>
         </Card>
+
+        <Box mt={2}>
+            <Typography fontWeight={800}>{product.title}</Typography>
+            <Typography fontWeight={400}>{`$${product.price}`}</Typography>
+        </Box>
     </Grid>
   )
 }
