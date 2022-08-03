2f721a9e57cc64b19f8141c2a2f0a874d451c5c5
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2022-08-03 12:37:47 -0500

    Renderizar ProductList en index

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -9,20 +9,16 @@ import { IProducts } from '../../interfaces';
 
 export const ProductCard:FC<ProductCardProps> = ({product}) => {
   return (
-    <Grid container spacing={4}>
-      {
-              <Grid item xs={6} sm={4} key={product.slug}>
-                  <Card>
-                      <CardActionArea>
-                          <CardMedia
-                              component='img'
-                              image={`products/${product.images[0] }`}
-                              alt={product.title}
-                          />
-                      </CardActionArea>
-                  </Card>
-              </Grid>
-      }
+    <Grid item xs={6} sm={4} key={product.slug}>
+        <Card>
+            <CardActionArea>
+                <CardMedia
+                    component='img'
+                    image={`products/${product.images[0] }`}
+                    alt={product.title}
+                />
+            </CardActionArea>
+        </Card>
     </Grid>
   )
 }
