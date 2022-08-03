7821c4507f749b17096f7d757a3bf0a8a36d5c1f
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2022-08-02 19:52:32 -0500

    Add estructura de App carpetas

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,14 @@
+import { Typography } from '@mui/material';
 import type { NextPage } from 'next'
+import { ShopLayout } from '../components/layout';
 
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <h1>Teslo Shop</h1>
-    </div>
+    <ShopLayout title={'Teslo-Shop'} pageDescription={'Encuentra los mejores productos aquí'}>
+      <Typography variant='h1' component='h1'>Tienda</Typography>
+      <Typography variant='h2'  sx={{ mb: 1 }}>Todos los productos</Typography>
+    </ShopLayout>
   )
 }
 
