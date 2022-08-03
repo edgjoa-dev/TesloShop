32b27ab9421716a094c87aa5970f92c815132270
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2022-08-03 10:47:49 -0500

    implementación catalogo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,10 @@
-import { Typography } from '@mui/material';
 import type { NextPage } from 'next'
+import { Card, CardActionArea, CardMedia, Grid, Typography } from '@mui/material';
+
 import { ShopLayout } from '../components/layout';
+import { initialData } from '../database/products';
+
+
 
 
 const Home: NextPage = () => {
@@ -8,6 +12,24 @@ const Home: NextPage = () => {
     <ShopLayout title={'Teslo-Shop'} pageDescription={'Encuentra los mejores productos aquí'}>
       <Typography variant='h1' component='h1'>Tienda</Typography>
       <Typography variant='h2'  sx={{ mb: 1 }}>Todos los productos</Typography>
+
+        <Grid container spacing={4}>
+          {
+            initialData.products.map(product => (
+              <Grid item xs={6} sm={4} key={product.slug}>
+                <Card>
+                  <CardActionArea>
+                    <CardMedia
+                    component='img'
+                    image={`products/${product.images[0] }`}
+                    />
+                  </CardActionArea>
+                </Card>
+              </Grid>
+            ))
+          }
+        </Grid>
+
     </ShopLayout>
   )
 }
