63889b60435cb774c5ccbb366e3c23d9b2d952e3
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2022-08-06 14:08:28 -0500

    Agregando campos al formulario de Checkout

--- pages/checkout/address.tsx
+++ pages/checkout/address.tsx
@@ -1,4 +1,4 @@
-import { Grid, TextField, Typography } from '@mui/material';
+import { FormControl, Grid, InputLabel, MenuItem, Select, TextField, Typography } from '@mui/material';
 import { ShopLayout } from '../../components/layout/ShopLayout';
 
 const AddressPage = () => {
@@ -11,6 +11,39 @@ const AddressPage = () => {
             <Grid container spacing={ 2 }>
                 <Grid item xs={12} sm={ 6 }>
                     <TextField label='Nombre' variant='filled' fullWidth/>
+                </Grid>
+                <Grid item xs={12} sm={ 6 }>
+                    <TextField label='Apellido' variant='filled' fullWidth/>
+                </Grid>
+                <Grid item xs={12} sm={ 6 }>
+                    <TextField label='Dirección' variant='filled' fullWidth/>
+                </Grid>
+                <Grid item xs={12} sm={ 6 }>
+                    <TextField label='Dirección 2 (opcional)' variant='filled' fullWidth/>
+                </Grid>
+                <Grid item xs={12} sm={ 6 }>
+                    <TextField label='Código Postal' variant='filled' fullWidth/>
+                </Grid>
+                <Grid item xs={12} sm={ 6 }>
+                    <TextField label='Ciudad' variant='filled' fullWidth/>
+                </Grid>
+                <Grid item xs={12} sm={ 6 }>
+                    <FormControl  fullWidth>
+                        <InputLabel>País</InputLabel>
+                        <Select
+                            variant='filled'
+                            label='País'
+                            value={ 1 }
+                        >
+                            <MenuItem value={ 1 }>México</MenuItem>
+                            <MenuItem value={ 2 }>Estados Unidos</MenuItem>
+                            <MenuItem value={ 3 }>Cánada</MenuItem>
+                            <MenuItem value={ 4 }>Guatemala</MenuItem>
+                        </Select>
+                    </FormControl>
+                </Grid>
+                <Grid item xs={12} sm={ 6 }>
+                    <TextField label='Ciudad' variant='filled' fullWidth/>
                 </Grid>
             </Grid>
         </ShopLayout>
