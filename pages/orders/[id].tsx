92d3b918552bd1e608e7d676118f1c08bfde3e7d
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2022-08-07 22:49:31 -0500

    Agregando Card

--- pages/orders/[id].tsx
+++ pages/orders/[id].tsx
@@ -1,9 +1,28 @@
 import { ShopLayout } from "../../components/layout"
+import { Box, Button, Card, CardContent, Divider, Grid, Link, Typography } from '@mui/material';
+import { CartList, OrderSummary } from '../../components/cart';
+import NextLink from 'next/link';
 
 const [id] = () => {
     return (
         <ShopLayout title='Resumen de Compra' pageDescription='Resumen de la compra'>
-    
+            <Typography variant='h1' component='h1'>Resumen de Compra</Typography>
+
+            <Grid container sx={{mt: 5}}>
+
+                <Grid item  xs={12} sm={7}>
+                    <CartList />
+                </Grid>
+
+                <Grid item  xs={12} sm={5}>
+
+                    <Card  className='summary-card'>
+
+
+
+                    </Card>
+                </Grid>
+            </Grid>
         </ShopLayout>
     )
 }
