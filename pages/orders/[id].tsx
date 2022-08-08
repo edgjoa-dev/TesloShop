b22381a608e1c18c535bde57f0ea61321a21983e
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2022-08-07 22:50:31 -0500

    Agregando contenido a Card

--- pages/orders/[id].tsx
+++ pages/orders/[id].tsx
@@ -18,7 +18,27 @@ const [id] = () => {
 
                     <Card  className='summary-card'>
 
+                    <CardContent>
+                            <Typography variant='h2'>Resumen de Compra:  3 productos</Typography>
 
+                            <Divider sx={{ my: 1 }} />
+
+                                <Box display='flex' justifyContent='space-between'>
+                                <Typography variant='subtitle1'>Dirección de entrega</Typography>
+                                    <NextLink href='/checkout/address' passHref>
+                                        <Link>
+                                            Editar
+                                        </Link>
+                                    </NextLink>
+                                </Box>
+
+                                <Typography>Edgar Joaquin Flores</Typography>
+                                <Typography>Villahermosa #10</Typography>
+                                <Typography>Chalco, Edo. de Méx</Typography>
+                                <Typography>+52 5530921195</Typography>
+
+
+                        </CardContent>
 
                     </Card>
                 </Grid>
