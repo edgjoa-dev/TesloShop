15840f9cc3f122f73ea6cb2c0b1b3980e332b8af
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2022-08-24 18:55:55 -0500

    Busqueda por queries implementado

---
+++ pages/api/search/[q].ts
@@ -0,0 +1,33 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+    message: string
+}
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+    switch (req.method) {
+        case 'GET':
+            return searchProducts(req, res)
+
+
+        default:
+            res.status(400).json({
+                message: 'Bad request'
+            })
+    }
+}
+
+function searchProducts(req: NextApiRequest, res: NextApiResponse<Data>) {
+
+    let {q = ''} = req.query
+
+    if(q.length === 0 ){
+        res.status(400).json({
+            message: 'Debe especificar el query de búsqueda.'
+        });
+    }
+
+    return res.status(200).json({
+        message: q.toString()
+    })
+}
