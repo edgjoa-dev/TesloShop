d6f56f8983749bc2738fe0b157a2c02543db3ed0
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2022-08-24 20:17:43 -0500

    Implementacion de SWR para peticiones http en Next

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,16 +6,27 @@ import { initialData } from '../database/products';
 import { ProductList } from '../components/products/ProductList';
 
 
+import useSWR from 'swr'
+const fetcher = (...args: [key: string]) => fetch(...args).then((res) => res.json())
 
 
 const Home: NextPage = () => {
+
+  const { data, error } = useSWR('/api/products', fetcher)
+
+  if (error) return <div>Failed to load</div>
+  if (!data) return <div>Loading...</div>
+
+  console.log({data});
+  
+
   return (
     <ShopLayout title={'Teslo-Shop'} pageDescription={'Encuentra los mejores productos aquí'}>
       <Typography variant='h1' component='h1'>Tienda</Typography>
       <Typography variant='h2'  sx={{ mb: 1 }}>Todos los productos</Typography>
 
       <ProductList
-        products={initialData.products as any}
+        products={data}
       />
 
     </ShopLayout>
