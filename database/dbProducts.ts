ae77f9619f682f141bd2e478e8a2db6b8ae2ccb9
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2022-08-30 15:21:51 -0500

    Busqueda por query en aside

--- database/dbProducts.ts
+++ database/dbProducts.ts
@@ -46,3 +46,13 @@ export const getProductsByTerm = async( term:string ): Promise<IProducts[]> => {
     return products;
 
 }
+
+export const getAllProducts = async(  ): Promise<IProducts[]> => {
+
+    await db.connect()
+    const products = await Product.find().lean();
+    await db.disconnect()
+
+    return JSON.parse(JSON.stringify(products));
+
+}
--- pages/search/[query].tsx
+++ pages/search/[query].tsx
@@ -1,6 +1,6 @@
 import type { NextPage, GetServerSideProps } from 'next'
 
-import { Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 
 import { ShopLayout } from '../../components/layout';
 import { dbProducts } from '../../database';
@@ -9,17 +9,28 @@ import { ProductList } from '../../components/products/ProductList';
 
 interface Props {
     products: IProducts[];
+    foundProducts: boolean;
+    query: string;
 }
 
 
 
-const SearchPage: NextPage<Props> = ({ products }) => {
+const SearchPage: NextPage<Props> = ({ products, foundProducts, query }) => {
 
 
     return (
         <ShopLayout title={'Teslo-Shop'} pageDescription={'Encuentra los mejores productos aquí'}>
         <Typography variant='h1' component='h1'>Buscar Productos</Typography>
-        <Typography variant='h2'  sx={{ mb: 1 }}>ABC--123</Typography>
+        {
+            foundProducts
+            ?<Typography variant='h2'  sx={{ mb: 1 }}> {query} </Typography>
+            :(
+                <Box display='flex'>
+                    <Typography variant='h2'  sx={{ mb: 1 }}> No se encontraron productos con: </Typography>
+                    <Typography variant='h2'  sx={{ ml: 1 }} color='InfoText'> {query} </Typography>
+                </Box>
+            )
+        }
 
         <ProductList products={products} />
         </ShopLayout>
@@ -39,10 +50,20 @@ export const getServerSideProps: GetServerSideProps = async ({params}) => {
     }
 
     let products = await dbProducts.getProductsByTerm(query);
+    const foundProducts = products.length > 0
+
+    if(!foundProducts){
+        //products = await dbProducts.getAllProducts();
+        products = await dbProducts.getProductsByTerm('shirts');
+    }
+
+
 
     return {
         props: {
-            products
+            products,
+            foundProducts,
+            query
         }
     }
 
