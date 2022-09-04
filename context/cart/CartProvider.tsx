f7ae69cd30712e90ca09dfa9a68975c191c225b9
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2022-09-04 14:24:36 -0500

    cambiar

--- context/cart/CartProvider.tsx
+++ context/cart/CartProvider.tsx
@@ -1,24 +1,33 @@
 import { FC, useEffect, useReducer } from 'react';
 import Cookie from 'js-cookie';
+import axios from 'axios';
 
-import { ICartProduct } from '../../interfaces';
+
+import { ICartProduct, IOrder, ShippingAddress } from '../../interfaces';
 import { CartContext, cartReducer } from './';
+import { tesloApi } from '../../api';
 
 export interface CartState {
+    isLoaded: boolean;
     cart: ICartProduct[];
     numberOfItems: number;
     subTotal: number;
     tax: number;
     total: number;
+
+    shippingAddress?: ShippingAddress;
 }
 
 
+
 const CART_INITIAL_STATE: CartState = {
+    isLoaded: false,
     cart: [],
     numberOfItems: 0,
     subTotal: 0,
     tax: 0,
     total: 0,
+    shippingAddress: undefined
 }
 
 
@@ -35,6 +44,27 @@ export const CartProvider:FC = ({ children }) => {
             dispatch({ type: '[Cart] - LoadCart from cookies | storage', payload: [] });
         }
     }, []);
+
+
+    useEffect(() => {
+
+        if ( Cookie.get('firstName')){
+            const shippingAddress = {
+                firstName : Cookie.get('firstName') || '',
+                lastName  : Cookie.get('lastName') || '',
+                address   : Cookie.get('address') || '',
+                address2  : Cookie.get('address2') || '',
+                zip       : Cookie.get('zip') || '',
+                city      : Cookie.get('city') || '',
+                country   : Cookie.get('country') || '',
+                phone     : Cookie.get('phone') || '',
+            }
+            
+            dispatch({ type:'[Cart] - LoadAddress from Cookies', payload: shippingAddress })
+        }
+    }, [])
+    
+
 
     
     useEffect(() => {
@@ -97,6 +127,67 @@ export const CartProvider:FC = ({ children }) => {
         dispatch({ type: '[Cart] - Remove product in cart', payload: product });
     }
 
+    const updateAddress = ( address: ShippingAddress ) => {
+        Cookie.set('firstName',address.firstName);
+        Cookie.set('lastName',address.lastName);
+        Cookie.set('address',address.address);
+        Cookie.set('address2',address.address2 || '');
+        Cookie.set('zip',address.zip);
+        Cookie.set('city',address.city);
+        Cookie.set('country',address.country);
+        Cookie.set('phone',address.phone);
+
+        dispatch({ type: '[Cart] - Update Address', payload: address });
+    }
+
+
+    const createOrder = async():Promise<{ hasError: boolean; message: string; }> => {
+
+        if ( !state.shippingAddress ) {
+            throw new Error('No hay dirección de entrega');
+        }
+
+        const body: IOrder = {
+            orderItems: state.cart.map( p => ({
+                ...p,
+                size: p.size!
+            })),
+            shippingAddress: state.shippingAddress,
+            numberOfItems: state.numberOfItems,
+            subTotal: state.subTotal,
+            tax: state.tax,
+            total: state.total,
+            isPaid: false
+        }
+
+
+        try {
+            
+            const { data } = await tesloApi.post<IOrder>('/orders', body);
+
+            dispatch({ type: '[Cart] - Order complete' });
+
+            return {
+                hasError: false,
+                message: data._id!
+            }
+
+
+        } catch (error) {
+            if ( axios.isAxiosError(error) ) {
+                return {
+                    hasError: true,
+                    message: error.response?.data.message
+                }
+            }
+            return {
+                hasError: true,
+                message : 'Error no controlado, hable con el administrador'
+            }
+        }
+
+    }
+
 
     return (
         <CartContext.Provider value={{
@@ -106,6 +197,10 @@ export const CartProvider:FC = ({ children }) => {
             addProductToCart,
             removeCartProduct,
             updateCartQuantity,
+            updateAddress,
+
+            // Orders
+            createOrder,
         }}>
             { children }
         </CartContext.Provider>
