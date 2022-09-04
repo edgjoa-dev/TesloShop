e0fe1673eac64b1084a38d5fc61ef41de617ed77
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2022-09-03 22:18:46 -0500

    Carrito de compras

--- context/cart/CartContext.tsx
+++ context/cart/CartContext.tsx
@@ -1,14 +1,20 @@
-import { createContext } from 'react'
+
+import { createContext } from 'react';
 import { ICartProduct } from '../../interfaces';
 
 
-interface contextProps{
+interface ContextProps {
     cart: ICartProduct[];
+    numberOfItems: number;
+    subTotal: number;
+    tax: number;
+    total: number;
 
-    //Methods
+    // Methods
     addProductToCart: (product: ICartProduct) => void;
     updateCartQuantity: (product: ICartProduct) => void;
+    removeCartProduct: (product: ICartProduct) => void;
 }
 
 
-export const CartContext = createContext({} as contextProps)
+export const CartContext = createContext({} as ContextProps );
--- context/cart/cartReducer.ts
+++ context/cart/cartReducer.ts
@@ -1,40 +1,66 @@
-import { CartState }  from './';
-import { ICartProduct } from "../../interfaces"
+
+import { CartState } from './';
+import { ICartProduct } from '../../interfaces';
 
 
-type CartActionType =
-|{ type: '[Cart] - LoadCart from cookies | storage', payload: ICartProduct[] }
-|{ type: '[Cart] - Updte products in cart', payload: ICartProduct[] }
-|{ type: '[Cart] - Change cart quantity', payload: ICartProduct }
+    type CartActionType =
+    | { type: '[Cart] - LoadCart from cookies | storage', payload: ICartProduct[] }
+    | { type: '[Cart] - Update products in cart', payload: ICartProduct[] }
+    | { type: '[Cart] - Change cart quantity', payload: ICartProduct }
+    | { type: '[Cart] - Remove product in cart', payload: ICartProduct }
+    | {
+        type: '[Cart] - Update order summary',
+        payload: {
+            numberOfItems: number;
+            subTotal: number;
+            tax: number;
+            total: number;
+        }
+    }
 
-export const cartReducer = (state: CartState, action:CartActionType): CartState => {
+
+export const cartReducer = ( state: CartState, action: CartActionType ): CartState => {
 
 switch (action.type) {
     case '[Cart] - LoadCart from cookies | storage':
-    return {
-    ...state,
-    cart: [...action.payload]
-    }
+        return {
+            ...state,
+            cart: [...action.payload]
+        }
 
-    case '[Cart] - Updte products in cart':
-    return {
-    ...state,
-    cart: [ ...action.payload]
-    }
+
+    case '[Cart] - Update products in cart':
+        return {
+            ...state,
+            cart: [ ...action.payload ]
+        }
+
 
     case '[Cart] - Change cart quantity':
-    return {
-    ...state,
-    cart: state.cart.map( product => {
-        if( product._id !== action.payload._id) return;
-        if( product.size !== action.payload.size) return;
+        return {
+            ...state,
+            cart: state.cart.map( product => {
+            if ( product._id !== action.payload._id ) return product;
+            if ( product.size !== action.payload.size ) return product;
+            return action.payload;
+            })
+        }
 
-        return action.payload;
-    })
-    }
 
+    case '[Cart] - Remove product in cart':
+        return {
+            ...state,
+            cart: state.cart.filter( product => !(product._id === action.payload._id && product.size === action.payload.size ))
+        }
 
-default:
-return state;
+    case '[Cart] - Update order summary':
+        return {
+            ...state,
+            ...action.payload
+        }
+
+    default:
+        return state;
 }
+
 }
