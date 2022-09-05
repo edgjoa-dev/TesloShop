42da85e0420f3d90f76c49c000969c18e6ac7262
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2022-09-04 20:35:38 -0500

    Manipulacion o al parseo de la cookie

--- context/cart/CartProvider.tsx
+++ context/cart/CartProvider.tsx
@@ -21,8 +21,12 @@ export const CartProvider:FC<Props> = ({ children }) => {
     const [state, dispatch] = useReducer( cartReducer, CART_INITIAL_STATE );
 
     useEffect( () => {
-        const cookieProducts = Cookie.get('cart') ? JSON.parse(Cookie.get('cart')!) : [];
-        dispatch({ type: '[Cart] - LoadCart from cookies | storage', payload: cookieProducts})
+        try {
+            const cookieProducts = Cookie.get('cart') ? JSON.parse(Cookie.get('cart')!) : [];
+            dispatch({ type: '[Cart] - LoadCart from cookies | storage', payload: cookieProducts})
+        } catch (error) {
+            dispatch({ type: '[Cart] - LoadCart from cookies | storage', payload: []})
+        }
     },[]);
 
     useEffect(()=>{
