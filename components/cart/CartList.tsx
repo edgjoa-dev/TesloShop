c13473b954cb987d89c7a21579df57c108a41a28
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2022-09-05 22:35:59 -0500

    eleccion de dos productos iguales en carrito de compras

--- components/cart/CartList.tsx
+++ components/cart/CartList.tsx
@@ -18,52 +18,52 @@ export const CartList:FC<Props> = ({editable = false}) => {
 
 
     return (
-                cart.map(product => (
-                    <Grid container spacing={2} key={product.slug}>
-                        <Grid item xs={3} sm={3}>
-                            <NextLink  href="/product/slug" passHref>
-                                <Link>
-                                    <CardActionArea>
-                                        <CardMedia
-                                            image={`/products/${ product.image }`}
-                                            component="img"
-                                            sx={{ borderRadius: '5px' }}
-                                        />
-                                    </CardActionArea>
-                                </Link>
-                            </NextLink>
-                        </Grid>
+    cart.map(product => (
+        <Grid container spacing={2} key={product.slug + product.size} sx={{mb:1}}>
+            <Grid item xs={3} sm={3}>
+                <NextLink  href={`/product/${product.slug}`} passHref>
+                    <Link>
+                        <CardActionArea>
+                            <CardMedia
+                                image={`/products/${ product.image }`}
+                                component="img"
+                                sx={{ borderRadius: '5px' }}
+                            />
+                        </CardActionArea>
+                    </Link>
+                </NextLink>
+            </Grid>
 
-                        <Grid item xs={7} sm={7}>
-                            <Box display='flex' flexDirection='column' >
-                                <Typography variant="body1">{product.title}</Typography>
-                                <Typography variant="body1">Talla: <strong> {product.size} </strong></Typography>
-                                {/* Condicional */}
-                                {
-                                    editable
-                                    ? <ItemCounter
-                                        currentValue={product.quantity}
-                                        maxValue={ 10 }
-                                        updatedQuantity={() => {}}
-                                    />
-                                    : <Typography variant="h6"> {product.quantity}{product.quantity > 1 ? 'productos' : 'producto'} </Typography>
-                                }
-                            </Box>
-                        </Grid>
+            <Grid item xs={7} sm={7}>
+                <Box display='flex' flexDirection='column' >
+                    <Typography variant="body1">{product.title}</Typography>
+                    <Typography variant="body1">Talla: <strong> {product.size} </strong></Typography>
+                    {/* Condicional */}
+                    {
+                        editable
+                        ? <ItemCounter
+                            currentValue={product.quantity}
+                            maxValue={ 10 }
+                            updatedQuantity={() => {}}
+                        />
+                        : <Typography variant="h6"> {product.quantity}{product.quantity > 1 ? 'productos' : 'producto'} </Typography>
+                    }
+                </Box>
+            </Grid>
 
-                        <Grid item xs={2} sm={2} display='flex' alignItems='center' flexDirection='column'>
-                            <Typography variant="subtitle1">${product.price}</Typography>
-                            {/* Editable */}
-                            {
-                                editable && (
-                                    <Button variant='text' color='error'>
-                                        Remover
-                                    </Button>
-                                )
-                            }
-                        </Grid>
-                    </Grid>
-                ))
+            <Grid item xs={2} sm={2} display='flex' alignItems='center' flexDirection='column'>
+                <Typography variant="subtitle1">${product.price}</Typography>
+                {/* Editable */}
+                {
+                    editable && (
+                        <Button variant='text' color='error'>
+                            Remover
+                        </Button>
+                    )
+                }
+            </Grid>
+        </Grid>
+    ))
             )
 }
 
