77e2b30f9cc5c0b1891aa8bb07ac9628ecdbaf4c
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2022-09-08 14:20:28 -0500

    Creacion de modelo de usuarios para login

---
+++ models/User.ts
@@ -0,0 +1,21 @@
+import mongoose, { Schema, model, Model } from 'mongoose'
+
+
+const userSchema = new Schema({
+
+    name:     { type: String, required: true },
+    email:    { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: {
+
+        type: String,
+        enum: {
+            values: ['admin', 'client'],
+            messages: '{VALUE} no es un role válido',
+            default: 'client',
+            required: true,
+        }
+    }
+}, {
+    timestamps: true,
+})
