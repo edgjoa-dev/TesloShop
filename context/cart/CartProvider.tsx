7a2d51855a8a21ee1daafc54e7c0fd335c463ddc
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2022-09-07 19:32:48 -0500

    Calcular precio con inpuestos

--- context/cart/CartProvider.tsx
+++ context/cart/CartProvider.tsx
@@ -37,11 +37,13 @@ export const CartProvider:FC<Props> = ({ children }) => {
 
         const numberOfItems = state.cart.reduce((prev, current) =>  current.quantity + prev , 0 );
         const subTotal = state.cart.reduce((prev, current) =>  (current.price * current.quantity) + prev, 0);
+        const taxRate = Number(process.env.NEXT_PUBLIC_TAX_RATE || 0);
 
 
         const orderSumary = {
             numberOfItems,
             subTotal,
+            tax: subTotal * taxRate,
         }
         console.log({orderSumary})
     },[state.cart])
