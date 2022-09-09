fb67484398996353d3416bda58f6a845b8192044
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2022-09-08 20:46:54 -0500

    Configuracion de JWT

--- utils/jwt.ts
+++ utils/jwt.ts
@@ -1,8 +1,17 @@
 import jwt from 'jsonwebtoken'
 
 
-export const singToken = () => {
+export const singToken = ( _id: string, email: string ) => {
 
     if(!process.env.JWT_SECRET_SEED)
     throw new Error ('No se encontro api secret key de JWT - Válidar variables de entorno')
+
+    return jwt.sign(
+        //payload
+        { _id, email },
+        //Seed
+        process.env.JWT_SECRET_SEED,
+        //options
+        { expiresIn: '1h' }
+    )
 }
