f465327c4c63d6e453795c9bdef7c4f08bb8a39e
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2022-09-10 21:06:08 -0500

    Colocar AuthProvider en app.tsx

--- context/auth/authReducer.ts
+++ context/auth/authReducer.ts
@@ -1,18 +1,32 @@
 import { IUser } from "../../interfaces"
-import { AuthState } = from './'
+import { AuthState } from './'
 
 
 type AuthActionType =
 |{ type: '[Auth] - Login', payload: IUser }
+|{ type: '[Auth] - Logout' }
 
 export const authReducer = (state = AuthState, action:AuthActionType): AuthState => {
 
-switch (action.type) {
-case '[Auth] - Login':
-return {
-...state
-}
-default:
-return state;
-}
+    switch (action.type) {
+
+    case '[Auth] - Login':
+        return {
+        ...state,
+        isLoggedIn: true,
+        user: action.payload
+        }
+
+    case '[Auth] - Logout':
+        return {
+        ...state,
+        isLoggedIn: false,
+        user: undefined
+        }
+
+
+
+    default:
+    return state;
+    }
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import type { AppProps } from 'next/app'
 import { ThemeProvider } from '@mui/material'
 import { lightTheme } from '../themes/light-theme';
 import { SWRConfig } from 'swr';
-import { CartProvider, UiProvider } from '../context';
+import { AuthProvider, CartProvider, UiProvider } from '../context';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return(
@@ -13,13 +13,15 @@ function MyApp({ Component, pageProps }: AppProps) {
         fetcher: (resource, init) => fetch(resource, init).then(res => res.json())
     }}
     >
-      <CartProvider>
-        <UiProvider>
-          <ThemeProvider theme={lightTheme} >
-            <Component {...pageProps} />
-          </ThemeProvider>
-        </UiProvider>
-      </CartProvider>
+      <AuthProvider>
+          <CartProvider>
+            <UiProvider>
+              <ThemeProvider theme={lightTheme} >
+                <Component {...pageProps} />
+              </ThemeProvider>
+            </UiProvider>
+          </CartProvider>
+      </AuthProvider>
     </SWRConfig>
   )
 }
