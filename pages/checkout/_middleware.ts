148dca4c53ae6295394d3743371ed75cf71e5c4c
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2022-09-15 19:15:52 -0500

    Middleware auth

--- pages/checkout/_middleware.ts
+++ pages/checkout/_middleware.ts
@@ -4,18 +4,20 @@ import { jwt } from "../../utils";
 
 
 
-export async function middleware( req: NextRequest, ev:NextFetchEvent ) {
 
-    const { token = '' } = req.cookies;
+export async function middleware ( req: NextRequest, ev: NextFetchEvent) {
+
+    const { token='' } = req.cookies;
 
     try {
-        await jwt.isValidToken( token );
+
+        await jwt.isValidToken( token)
         return NextResponse.next();
+
     } catch (error) {
         const requestedPage = req.page.name;
-        return NextResponse.redirect(`/auth/login?p=${requestedPage}`)
+        return NextResponse.redirect(`/auth/login?p=/${requestedPage}`)
     }
 
-    }
 
 }
