dbbf1f90b25ae3dc2f09289fc4f3ec76914cea67
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2022-09-23 12:49:16 -0500

    Next Auth - configuracion middleware login

--- pages/checkout/_middleware.ts
+++ pages/checkout/_middleware.ts
@@ -1,23 +1,29 @@
 import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
-import { jwt } from "../../utils";
+import { getToken } from 'next-auth/jwt'
+//import { jwt } from "../../utils";
 
 
 
 
 
-export async function middleware ( req: NextRequest, ev: NextFetchEvent) {
+export async function middleware ( req: NextRequest | any, ev: NextFetchEvent) {
 
-    const { token='' } = req.cookies;
+    const session = await getToken({ req, secret: process.env.NEXTAUTH_SECRET});
 
-    try {
-
-        await jwt.isValidToken( token)
-        return NextResponse.next();
-
-    } catch (error) {
+    if(!session){
         const requestedPage = req.page.name;
-        return NextResponse.redirect(`/auth/login?p=/${requestedPage}`)
+        return NextResponse.redirect(` /auth/login?p=${ requestedPage }`)
     }
 
+    return NextResponse.next();
+
+    // const { token='' } = req.cookies;
+    // try {
+    //     await jwt.isValidToken( token)
+    //     return NextResponse.next();
+    // } catch (error) {
+    //     const requestedPage = req.page.name;
+    //     return NextResponse.redirect(`/auth/login?p=/${requestedPage}`)
+    // }
 
 }
