23adc31e0c32b7942cd3e30f7ab151ba25f0b3d5
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2022-09-30 09:05:27 -0500

    creacion de create order api

---
+++ pages/api/orders/index.ts
@@ -0,0 +1,22 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+    message: string
+}
+
+    export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+
+        switch (req.method) {
+            case 'POST':
+                return createOrder( req, res );
+
+            default:
+                return res.status(404).json({ message: 'Invalid method' });
+        }
+
+    }
+
+    function createOrder(req: NextApiRequest, res: NextApiResponse<Data>) {
+    res.status(200).json({ message: 'Create order from api' })
+
+}
--- pages/checkout/summary.tsx
+++ pages/checkout/summary.tsx
@@ -37,65 +37,65 @@ const SummaryPage = () => {
     const { firstName, lastName, address, address2 = '', city, country, phone, zip } = shippingAddress;
 
     return (
-    <ShopLayout title='Resumen de Compra' pageDescription='Resumen de la compra'>
+<ShopLayout title='Resumen de Compra' pageDescription='Resumen de la compra'>
 
-            <Typography variant='h1' component='h1'>Resumen de Compra</Typography>
+    <Typography variant='h1' component='h1'>Resumen de Compra</Typography>
 
-            <Grid container sx={{mt: 5}}>
+    <Grid container sx={{mt: 5}}>
 
-                <Grid item  xs={12} sm={7}>
-                    <CartList editable={false} />
-                </Grid>
+        <Grid item  xs={12} sm={7}>
+            <CartList editable={false} />
+        </Grid>
 
-                <Grid item  xs={12} sm={5}>
+        <Grid item  xs={12} sm={5}>
 
-                    <Card  className='summary-card'>
+            <Card  className='summary-card'>
 
-                        <CardContent>
-                            <Typography variant='h2'>Resumen de Compra: ({numberOfItems} {numberOfItems === 1 ? 'producto' : 'productos'} )</Typography>
-                            <Divider sx={{ my: 1 }} />
+                <CardContent>
+                    <Typography variant='h2'>Resumen de Compra: ({numberOfItems} {numberOfItems === 1 ? 'producto' : 'productos'} )</Typography>
+                    <Divider sx={{ my: 1 }} />
 
-                                <Box display='flex' justifyContent='space-between'>
-                                <Typography variant='subtitle1'>Dirección de entrega</Typography>
-                                    <NextLink href='/checkout/address' passHref>
-                                        <Link>
-                                            Editar
-                                        </Link>
-                                    </NextLink>
-                                </Box>
+                        <Box display='flex' justifyContent='space-between'>
+                        <Typography variant='subtitle1'>Dirección de entrega</Typography>
+                            <NextLink href='/checkout/address' passHref>
+                                <Link>
+                                    Editar
+                                </Link>
+                            </NextLink>
+                        </Box>
 
-                                <Typography>{firstName} {lastName}</Typography>
-                                <Typography>{address}, {address2}</Typography>
-                                <Typography>{city}, {zip}, {country}</Typography>
-                                <Typography>{phone}</Typography>
+                        <Typography>{firstName} {lastName}</Typography>
+                        <Typography>{address}, {address2}</Typography>
+                        <Typography>{city}, {zip}, {country}</Typography>
+                        <Typography>{phone}</Typography>
 
-                            <Divider sx={{ my: 1 }} />
-                                <Box display='flex' justifyContent='space-between'>
-                                    <Typography variant='subtitle1'>Resumen de compra</Typography>
-                                    <NextLink href='/cart' passHref>
-                                        <Link>
-                                            Editar
-                                        </Link>
-                                    </NextLink>
-                                </Box>
-                                <OrderSummary />
+                    <Divider sx={{ my: 1 }} />
+                        <Box display='flex' justifyContent='space-between'>
+                            <Typography variant='subtitle1'>Resumen de compra</Typography>
+                            <NextLink href='/cart' passHref>
+                                <Link>
+                                    Editar
+                                </Link>
+                            </NextLink>
+                        </Box>
+                        <OrderSummary />
 
-                            <Box  sx={{mt: 3}}>
-                                <Button
-                                color='secondary'
-                                className='circular-btn'
-                                fullWidth
-                                onClick={onCreateOrder}
-                                >
-                                    Confirmar Compra
-                                </Button>
-                            </Box>
-                        </CardContent>
+                    <Box  sx={{mt: 3}}>
+                        <Button
+                        color='secondary'
+                        className='circular-btn'
+                        fullWidth
+                        onClick={onCreateOrder}
+                        >
+                            Confirmar Compra
+                        </Button>
+                    </Box>
+                </CardContent>
 
-                    </Card>
-                </Grid>
-            </Grid>
-        </ShopLayout>
+            </Card>
+        </Grid>
+    </Grid>
+</ShopLayout>
     )
 }
 
