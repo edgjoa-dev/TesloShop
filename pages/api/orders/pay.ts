74e99e28d7ae3206e60650fc925d762b41a81b9c
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2022-11-10 19:34:04 -0600

    obtener paypal OAuth Token api/orders/pay

--- pages/api/orders/pay.ts
+++ pages/api/orders/pay.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import axios from 'axios';
+import axios, { Axios } from 'axios';
 
 type Data = {
     message: string
@@ -23,25 +23,34 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<Data>)
         const PAYPAL_CLIENT_ID = process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID;
         const PAYPAL_SECRET    = process.env.PAYPAL_SECRET;
 
+        const base64Token = Buffer.from(`${PAYPAL_CLIENT_ID}:${PAYPAL_SECRET}`, 'utf-8').toString('base64');
         const body = new URLSearchParams( 'grant_type=client_credentials' );
 
         try {
-
             const {data} = await axios.post(process.env.PAYPAL_OAUTH_URL || '', body, {
                 headers: {
+                    'Authorization': `Basic ${base64Token}`,
                     'Content-Type': 'application/x-www-form-urlencoded'
                 }
             })
+            return data.access_token;
 
         } catch (error) {
-
+            if( axios.isAxiosError(error) ) {
+                console.log(error.response?.data);
+            }else{
+                console.log(error);
+            }
+            return null;
         }
-
     }
 
+    const  payOrder = async(req: NextApiRequest, res: NextApiResponse<Data>) => {
 
-    const  payOrder = (req: NextApiRequest, res: NextApiResponse<Data>) => {
-
-        return res.status(200).json({ message: 'Orden pagada' })
+        const paypalBearerToken = await getPaypalBearerToken();
+        if( !paypalBearerToken ){
+            return res.status(400).json({ message: 'No se pudo generar el token de paypal' })
+        }
+        return res.status(200).json({ message: paypalBearerToken })
 
 }
