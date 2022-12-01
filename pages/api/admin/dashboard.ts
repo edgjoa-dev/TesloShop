64c86af32ae24d5c293e1ad22f74345ebb3d0f66
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2022-11-30 19:00:39 -0600

    consultar data de DB para traer info de RestAPi

--- pages/api/admin/dashboard.ts
+++ pages/api/admin/dashboard.ts
@@ -16,12 +16,28 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
 
     await db.connect();
 
-        const numberOfOrders = await Order.count();
-        const paidOrders = await Order.find({ isPaid: true }).count();
-        const numberOfClients = await User.find({ role: 'client' }).count();
-        const numberOfProducts = await Product.count();
-        const productsWithNoInventory = await Product.find({ inStock: 0 }).count();
-        const lowInventory = await Product.find({ inStock: {$let: 10} }).count();
+        // const numberOfOrders = await Order.count();
+        // const paidOrders = await Order.find({ isPaid: true }).count();
+        // const numberOfClients = await User.find({ role: 'client' }).count();
+        // const numberOfProducts = await Product.count();
+        // const productsWithNoInventory = await Product.find({ inStock: 0 }).count();
+        // const lowInventory = await Product.find({ inStock: {$let: 10} }).count();
+
+        const [
+            numberOfOrders,
+            paidOrders,
+            numberOfClients,
+            numberOfProducts,
+            productsWithNoInventory,
+            lowInventory,
+        ] = await Promise.all([
+            Order.count(),
+            Order.find({ isPaid: true }).count(),
+            User.find({ role: 'client' }).count(),
+            Product.count(),
+            Product.find({ inStock: 0 }).count(),
+            Product.find({ inStock: {$let: 10} }).count(),
+        ])
 
     await db.disconnect();
 
