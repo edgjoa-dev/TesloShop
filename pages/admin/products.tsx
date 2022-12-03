e3e13f8cc23c0a591a7b549081df769195e0048e
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2022-12-02 18:35:32 -0600

    Feature: implementación listado de productos

--- pages/admin/products.tsx
+++ pages/admin/products.tsx
@@ -4,61 +4,42 @@ import { ConfirmationNumberOutlined } from '@mui/icons-material';
 import { Grid, Chip } from '@mui/material';
 import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
 import useSWR from 'swr';
-import { IOrder } from '../../interfaces/order';
-import { IUser } from '../../interfaces/user';
+import { IProducts } from "../../interfaces";
 
 const columns: GridColDef[] = [
-    { field: 'id', headerName: 'Order ID', width: 300 },
-    { field: 'email', headerName: 'Correo', width: 250 },
-    { field: 'name', headerName: 'Nombre Completo', width: 250 },
-    { field: 'total', headerName: 'Monto Total', width: 150 },
-    {
-        field: 'isPaid',
-        headerName: 'Pagada',
-        renderCell: ({row}: GridValueGetterParams ) => {
-            return row.isPaid
-            ?(<Chip variant="outlined" label="Pagada" color="success" />)
-            :(<Chip variant="outlined" label="Pendiente" color="error" />)
-        }
-    },
-    { field: 'noProducts', headerName: 'No.Productos',  align: 'center', width: 130},
-    {
-        field: 'check',
-        headerName: 'Ver Orden',
-        renderCell: ({row}: GridValueGetterParams ) => {
-            return (
-                <a href={`/admin/orders/${ row.id }`} target="_blank" rel="noreferrer" >
-                    Ver orden
-                </a>
-            )
-        }
-    },
-    { field: 'createdAt', headerName: 'Creada en:', width: 300},
+    { field: 'img', headerName: 'Foto' },
+    { field: 'title', headerName: 'title', width: 250 },
+    { field: 'gender', headerName: 'Género' },
+    { field: 'type', headerName: 'Tipo' },
+    { field: 'inStock', headerName: 'Invenatrio' },
+    { field: 'price', headerName: 'Precio' },
+    { field: 'sizes', headerName: 'Tallas', width: 250 },
 
 ]
 
 
 const ProductPage =  () => {
 
-    const { data, error } = useSWR< IOrder[]>('/api/admin/orders')
+    const { data, error } = useSWR< IProducts[]>('/api/admin/products')
 
     if( !data && !error ) return(<></>);
 
-    const rows = data!.map( order => ({
-        id: order._id,
-        email: (order.user as IUser ).email,
-        name: (order.user as IUser ).name,
-        total: order.total,
-        isPaid: order.isPaid,
-        noProducts: order.numberOfItems,
-        createdAt: order.createdAt,
+    const rows = data!.map( product => ({
+        id: product._id,
+        img: product.images[0],
+        title: product.title,
+        gender: product.gender,
+        type: product.type,
+        inStock: product.inStock,
+        price: product.price,
+        sizes: product.sizes,
     }))
 
 
     return (
         <AdminLayout
-            title={'Ordenes'}
-            subTitle={'Mantenimiento de ordenes'}
+            title={`Productos ${data?.length}`}
+            subTitle={'Mantenimiento de productos'}
             icon={ <ConfirmationNumberOutlined /> }
         >
         <Grid container className="fadeIn">
