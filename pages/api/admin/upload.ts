69e75efb5e34d62df0df602c0eb6b6125b44d59e
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2022-12-04 19:58:22 -0600

    feature. almacenamiento de archivos en el file system

--- pages/api/admin/upload.ts
+++ pages/api/admin/upload.ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import formidable from 'formidable'
+import fs from 'fs'
 
 
 type Data = {
@@ -24,8 +25,30 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<Data>)
 
 }
 
+const saveFile = async( file: formidable.File ) => {
+    const data = fs.readFileSync( file.filepath);
+    fs.writeFileSync( `./public/${ file.originalFilename }`, data);
+
+    fs.unlinkSync( file.filepath );
+    return;
+
+}
+
     const parseFiles = async( req: NextApiRequest ) => {
-        
+        return new Promise((resolve, reject ) => {
+            const form = new formidable.IncomingForm();
+            form.parse( req, async( err, fields, files )=> {
+                console.log({ err, fields, files })
+
+                if( err ){
+                    return reject( err );
+                }
+
+                await saveFile( files.file as formidable.File )
+                return(true);
+            })
+
+        })
     }
 
 const uploadFile = async(req: NextApiRequest, res: NextApiResponse<Data>) => {
